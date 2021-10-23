676e3380178d8fb695b76dd1679d08bdd72faabc
Kanu Peace <kanupeace85@gmail.com>
2021-10-23 15:03:34 +0100

    web social platform

--- resources/views/web/layouts/includes/footer.blade.php
+++ resources/views/web/layouts/includes/footer.blade.php
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-lg-12">
                 <ul class="social-icons">
-                    <li><a href="#">facebook</a></li>
-                    <li><a href="#">twitter</a></li>
+                    <li><a href="https://www.facebook.com/petpea.downloader">facebook</a></li>
+                    <li><a href="https://instagram.com/pet_pea_downloader?utm_medium=copy_link">instagram</a></li>
                     {{-- <li><a href="#">Behance</a></li> --}}
                     <li><a href="#">linkedin</a></li>
                     {{-- <li><a href="#">Dribbble</a></li> --}}
--- resources/views/web/layouts/includes/head.blade.php
+++ resources/views/web/layouts/includes/head.blade.php
@@ -10,7 +10,7 @@
     <link
         href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap"
         rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="{{$admin_assets}}/assets/img/m-icon.jpeg" />
+    <link rel="icon" type="image/x-icon" href="{{$admin_assets}}/assets/img/m-icon.jpeg" >
 
 
     <title>{{ config('', 'Media-Creator') }}</title>
