1a7a9b7bde2ef9a1f552a6c7a719d8168539d868
peter-say <iriogbepeter22@gmail.com>
2021-11-03 17:39:42 +0100

    saving

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -8,13 +8,13 @@ use Illuminate\Http\Request;
 
 class UserPostController extends Controller
 {
-    public function index( User $user)
-    {
+    // public function index( User $user)
+    // {
         
-        $posts = $user->posts()->with(['user'])->paginate(5);
-       return view('web.singleUser', [
-           'user' => $user,
-           'posts' => $posts,
-       ]);
-    }
+    //     $posts = $user->posts()->with(['user'])->paginate(5);
+    //    return view('web.singleUser', [
+    //        'user' => $user,
+    //        'posts' => $posts,
+    //    ]);
+    // }
 }
