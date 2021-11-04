c3c1e67f2236f12f6760d8adb433013305048cc4
peter-say <iriogbepeter22@gmail.com>
2021-11-04 16:05:52 +0100

    red button remove from dashboard

--- public/admin/assets/js/app.js
+++ public/admin/assets/js/app.js
@@ -130,26 +130,8 @@ var App = function() {
             for (var i = 0; i < form.length; i++) {
                 var obj = form[i];
                 switch (obj.type) {
-                    case "button":
-                        var button = document.createElement('button');
-                        var textButton = document.createTextNode(obj.label);
-                        button.innerHTML = '<svg style="width: 15px; height: 15px; stroke-width: 2; vertical-align: middle;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up"><line x1="12" y1="19" x2="12" y2="5"></line><polyline points="5 12 12 5 19 12"></polyline></svg>';
-                        button.style.cssText = " margin-left: 6px;padding: 6px 9px; display: none; border: none;box-shadow: 0 10px 20px -10px #4801FF; background-image: linear-gradient(-225deg, #AC32E4 0%, #7918F2 48%, #4801FF 100%);";
-                        button.classList.add('btn', 'btn-secondary', 'scroll-top-btn');
-                        button.appendChild(textButton);
-                        element.appendChild(button);
-                        break;
+                   
             
-                        case "anchor":
-                            var anchor = document.createElement('a');
-                            var textanchor = document.createTextNode(obj.label);
-                            anchor.setAttribute('href',"");
-                            anchor.style.cssText = "border: none; background-image: linear-gradient(to right, #ff0844 20%, #ffb199 141%);box-shadow: 0 10px 20px -10px #ff0844;";
-                            anchor.classList.add('btn', 'btn-primary', 'buy-btn');
-                            anchor.target = obj.target;
-                            anchor.appendChild(textanchor);
-                            element.appendChild(anchor);
-                            break;
                 }
                 var div = document.querySelector("body");
                 div.appendChild(element);
