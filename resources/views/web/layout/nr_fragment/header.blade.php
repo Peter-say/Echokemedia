29f459a7409f3e4c09e9ea2954564891074032e6
KanuPeace <kanupeace85@gmail.com>
2022-05-09 16:57:16 +0100

    new releases changes

--- resources/views/web/layout/nr_fragment/header.blade.php
+++ resources/views/web/layout/nr_fragment/header.blade.php
@@ -104,7 +104,7 @@
 		</div><!-- close #header-user-profile-click -->
 		<div id="header-user-profile-menu">
 			<ul>
-				<li><a href="dashboard-favorites.html"><span class="icon-Favorite-Window"></span>My Favorites</a></li>
+				<li><a href="#"><span class="icon-Favorite-Window"></span>My Favorites</a></li>
 				<li><a href="{{ route('media.contact') }}"><span class="icon-Life-Safer"></span>Help/Support</a></li>
 				<li><a href="/"><span class="fa fa-home"></span>Home</a></li>
 			</ul>
