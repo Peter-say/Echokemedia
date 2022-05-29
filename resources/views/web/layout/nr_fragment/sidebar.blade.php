a0d44ed4d8556686e0098640b0026cd50b814d9d
KanuPeace <kanupeace85@gmail.com>
2022-05-29 13:00:09 +0100

    sidebar

--- resources/views/web/layout/nr_fragment/sidebar.blade.php
+++ resources/views/web/layout/nr_fragment/sidebar.blade.php
@@ -1,24 +1,25 @@
 <nav id="sidebar-nav">
     <!-- Add class="sticky-sidebar-js" for auto-height sidebar -->
     <ul id="vertical-sidebar-nav" class="sf-menu">
-        <li class="normal-item-pro current-menu-item">
-            <a href="{{route('media.newreleases')}}">
-                <span class="icon-Old-TV"></span>
-                Music
-            </a>
-        </li>
-        <li class="normal-item-pro">
-            <a href="{{route('media.videos')}}">
-                <span class="icon-Reel"></span>
-                Music Video
-            </a>
-        </li>
         <li class="normal-item-pro">
             <a href="#">
                 <span class="icon-Movie"></span>
                 Playlists
             </a>
         </li>
+        <li class="normal-item-pro current-menu-item">
+            <a href="{{ route('media.newreleases') }}">
+                <span class="icon-Old-TV"></span>
+                Music
+            </a>
+        </li>
+        <li class="normal-item-pro">
+            <a href="{{ route('media.videos') }}">
+                <span class="icon-Reel"></span>
+                Music Video
+            </a>
+        </li>
+
         <li class="normal-item-pro">
             <a href="#">
                 <span class="icon-Movie-Ticket"></span>
