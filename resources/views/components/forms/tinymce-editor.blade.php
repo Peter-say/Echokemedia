7eee674da080d7d3d2de8be0bdf30aca2852dd9d
KanuPeace <kanupeace85@gmail.com>
2022-05-31 14:21:26 +0100

    tinymce

--- resources/views/components/forms/tinymce-editor.blade.php
+++ resources/views/components/forms/tinymce-editor.blade.php
@@ -1,2 +1,2 @@
 
-  <textarea id="myeditorinstance" type="text" name="content_desccription" class="form-control">{{old("content_desccription")}}</textarea>
+<textarea id="myeditorinstance" type="text" name="content_desccription" class="form-control">{{old("content_desccription")}}</textarea>
--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -2,10 +2,10 @@
 
 <script>
     tinymce.init({
-        selector: 'textarea', // Replace this CSS selector to match the placeholder element for TinyMCE
-        plugins: 'code table lists',
-        plugins: 'image',
-        toolbar: 'image',
-        toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+        selector: "textarea", // Replace this CSS selector to match the placeholder element for TinyMCE
+        plugins: "code table lists",
+        plugins: "image",
+        toolbar: "image",
+        toolbar: "undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table"
     });
 </script>
